f76d0039e8a75e9bf410631406653064beaca96f
touraye <touraye07@gmail>
2024-12-15 21:20:06 +0000

    initial page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,83 @@
+import PopularBooks from "@/components/PopularBook"
+import NewCollections from "@/components/NewCollection";
+import Book from "@/components/Book";
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
 
-export default function Home() {
+export default function Home({ children }: {
+  children: React.ReactNode
+}) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section className="flex flex-col md:flex-row  items-start gap-2 md:gap-6 py-4 px-4">
+      <div>
+        <div className="mb-6">
+          <h1 className="text-3xl mb-2 	font-bold">Happy reading, <br></br>Ahmed</h1>      
+          <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati minus qui vitae molestias, iste eum et dolorem deleniti voluptatum repellendus aliquam unde ipsa accusamus.</p>
+          <Link href="#" className="rounded-full dark:bg-gray-300 dark:text-gray-900  bg-gray-900 text-white py-2 px-6">
+            Start reading
+            <ArrowUpRight className="inline-block ml-2" />
+          </Link>
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <PopularBooks />
+        <NewCollections />
+      </div>
+      
+      <div>
+        <Book
+          img="/assets/book3.webp"
+          title="The Chamber of Secrets"
+          availability={true}
+          imprints={30}
+          page={190}
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati vero repellendus sapiente sunt placeat eum."
+          borrowed={20}          
+        />
+
+        <div className="mt-4">
+          <h2 className="text-2xl font-semibold">Popular Authors</h2>
+          <ul className="flex flex-col gap-3 my-4">
+            <li className="flex items-start gap-3">              
+                <Image 
+                  className="rounded-full"
+                  width={50}
+                  height={100}
+                  src="/assets/user-profile1.avif"
+                  alt="author image"
+                />
+              <div>
+                <h3 className="font-semibold">Roberto Jordan</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum dolore sunt aperiam.</p>
+                <div className="flex items-center justify-between mt-2">
+                  <p>Something</p>
+                  <p>Book: 12</p>
+                </div>
+              </div>
+            </li>
+
+             <li className="flex items-start gap-3">              
+                <Image 
+                  className="rounded-full"
+                  width={50}
+                  height={100}
+                  src="/assets/user-profile1.avif"
+                  alt="author image"
+                />
+              <div>
+                <h3 className="font-semibold">Henna Henry</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum dolore sunt aperiam.</p>
+                <div className="flex items-center justify-between mt-2">
+                  <p className="">Something</p>
+                  <p className="font-thin">Book: 5</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+          
+        </div>
+      </div>      
+    </section>
   );
 }
