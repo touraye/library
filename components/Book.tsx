49a1f28b82995265ddc8949a3232c373f99d90bb
touraye <touraye07@gmail>
2024-12-15 21:21:42 +0000

    book component: reusable component that will use throughout

---
+++ components/Book.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const Book = (
+    { img, title, availability, imprints, page, description, borrowed }:
+    { img: string, title: string, availability: boolean, imprints: number, page: number, description: string, borrowed: number }) => {
+    return (
+        <section className="flex flex-col md:flex-row items-start gap-2">
+        <Image src={img} width={100} height={100} alt={title} />
+        <div>
+            <h2 className="text-2xl mb-2">{title}</h2>
+            <p>Available: {availability ? "Yes" : "No"}</p>
+            <p>Imprints: {imprints}</p>
+            <p className="my-2">{description}</p>
+            <p className="mr-auto">Author: {borrowed}</p>
+        </div>
+    </section>);
+}
+ 
+export default Book;
