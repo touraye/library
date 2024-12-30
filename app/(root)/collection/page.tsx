4f1017977ab019a2c6c12b1fbcbca9bc99086b94
touraye <touraye07@gmail>
2024-12-30 18:29:13 +0000

    collection setup

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import Author from "@/components/Author"
+import Gallery from "@/components/Gallery"
+import Showcase from "@/components/Showcase"
+import { MoveLeft, MoveRight } from "lucide-react"
+import Image from "next/image"
 
 const Page = () => {
   return (
-    <div>
-      <h1>Collection...</h1>
-    </div>
+    <>    
+    <section className="flex flex-col md:flex-row items-start gap-4 pt-4">
+        <div className="flex-1">
+          <Showcase 
+          title="Keep the story going..."
+          name="Admeh"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus optio omnis velit, repellat magni delectus."
+          url="#"
+          urlName="Start reading"      
+        />          
+      </div>
+
+        {/* author */}
+        <div className="flex-1">
+          <Author
+            image="/assets/user-profile1.avif"
+            name="George PR Martin"
+            designation="author"
+            action="..."
+            description="Fire and blood tells the story of the Tragaryen dynasty in Westeros, chronicling conquest of the seven Kingdoms by House Tragaryen. It also cover Tragaryen civil war knows as the Dance of the Dragon"
+          />
+
+        </div>
+      
+      </section>    
+
+      {/* Gallery */}
+      <Gallery />
+    </>    
   )
 }
 
